809078dd8be11a5b2e5aff1aa4d54c26ff174937
Silvio Franco <silviofrancoms@gmail.com>
2021-10-28 02:41:16 -0400

    Produto e associação muitos para muitos

---
+++ src/main/java/com/silviofranco/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.silviofranco.cursomc.repositories;
+
+import com.silviofranco.cursomc.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
