aa226d1b64384be0e886f84c37762d71265f9fe7
Silvio Franco <silviofrancoms@gmail.com>
2021-11-29 10:50:01 -0400

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/silviofranco/cursomc/domain/Categoria.java
+++ src/main/java/com/silviofranco/cursomc/domain/Categoria.java
@@ -10,6 +10,7 @@ import java.util.Objects;
 
 @Entity
 public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/java/com/silviofranco/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,10 @@
+package com.silviofranco.cursomc.repositories;
+
+import com.silviofranco.cursomc.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
